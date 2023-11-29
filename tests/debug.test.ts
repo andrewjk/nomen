964c3c33bcd7f4c9f254b6d6557e4cfda5d4a6e0
Andrew K <andrewjk@users.noreply.github.com>
2023-11-29 18:54:38 +1100

    Chore: add test debugging

---
+++ tests/debug.test.ts
@@ -0,0 +1,20 @@
+import { test } from "uvu";
+
+/*
+import assert from "uvu/assert";
+import parse from "../src/parse";
+import tokenize from "../src/tokenize";
+import type AccessNode from "../src/types/AccessNode";
+import type AssignmentNode from "../src/types/AssignmentNode";
+import type DeclarationNode from "../src/types/DeclarationNode";
+import type FieldAccessNode from "../src/types/FieldAccessNode";
+import type InvocationNode from "../src/types/InvocationNode";
+import type ValueNode from "../src/types/ValueNode";
+import trim_test_data from "./trim_test_data";
+*/
+
+test("debugging test", () => {
+  // Copy your test in here
+});
+
+test.run();
