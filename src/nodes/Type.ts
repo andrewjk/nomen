f6e52731fce1a9fc62af66692296dff8d86bce2e
Andrew K <andrewjk@users.noreply.github.com>
2023-12-28 14:44:49 +1100

    Refactor: tidy up

--- src/nodes/Type.ts
+++ src/nodes/Type.ts
@@ -1,15 +1,13 @@
 import BaseNode from "./BaseNode";
-import DeclarationNode from "./DeclarationNode";
-import FunctionNode from "./FunctionNode";
 
 export default class Type {
   name: string;
-  is_array: boolean;
+  is_array?: boolean;
   length?: BaseNode;
 
   constructor(name: string, is_array?: boolean, length?: BaseNode) {
     this.name = name;
-    this.is_array = is_array || false;
+    this.is_array = is_array;
     this.length = length;
   }
 }
