1eae83d9589dc77f5861667409e5f33da0ef6409
Andrew K <andrewjk@users.noreply.github.com>
2026-06-01 11:17:10 +1000

    Test: fix list test auditing

--- test/audit_runtime.c
+++ test/audit_runtime.c
@@ -10,6 +10,20 @@ void *echo_malloc_wrap(unsigned long size) {
 	return ptr;
 }
 
+void *echo_calloc_wrap(unsigned long count, unsigned long size) {
+	void *ptr = calloc(count, size);
+	echo_malloc_count++;
+	return ptr;
+}
+
+void *echo_realloc_wrap(void *old_ptr, unsigned long size) {
+	if (!old_ptr) {
+		echo_malloc_count++;
+	}
+	void *ptr = realloc(old_ptr, size);
+	return ptr;
+}
+
 void echo_free_wrap(void *ptr) {
 	free(ptr);
 	echo_malloc_count--;
